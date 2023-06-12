c99b4c0d8479a2c62049f2e3b64143a44e1be06b
tamirzitman <tamirzitman@gmail.com>
2023-06-13 00:08:15 +0300

    flow, helper function added, data struc

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Genetic-Algorithm
-MMN11 | Operations Management 2 | The Open University
+<!-- MMN11 | Operations Management 2 | The Open University -->
+
+This repository contains the implementation of a Genetic Algorithm for solving a job scheduling problem with the objective of minimizing delay cost.
+
+Problem Description
+The problem involves scheduling a set of jobs on a single machine. The objective is to minimize the total delay cost.
+Follow the instructions below to run the Python code and solve the problem.
+
+# Usage 
+Clone the repository to your local machine or download the script file (genetic_algorithm.py).
+
+Prepare the input data:
+
+Create a text file (input.txt) with the job details in a predefined format. Each line should represent a job and include its relevant information.
+The format for each line should be: job_id, processing_time, due_date, weight.  
+
+Example:
+```
+Job1, 5, 10, 3
+Job2, 3, 8, 2
+Job3, 4, 6, 1
+```
+Open a terminal or command prompt and navigate to the directory where the script is located.
+
+Run the script using the following command:
+```
+python genetic_algorithm.py input.txt
+```
+The script will execute the Genetic Algorithm and output the best job schedule found, along with the corresponding delay cost.
+
+Adjust the algorithm's parameters in the script (if necessary):
+
+Population size: Modify the 'population_size' variable in the script to change the size of the population.
+Mutation rate: Adjust the 'mutation_rate' variable to control the rate of mutations in the algorithm.
+
+**Note: Make sure the input file is in the correct format and contains valid job details. The script assumes the input is well-formed and does not perform extensive error checking.**
+
+# Additional Information
+The script uses a fitness function based on the delay cost to evaluate the quality of each job schedule.
+The algorithm employs selection, crossover, and mutation operations to evolve the population towards better solutions.
+The output includes the best job schedule found and the corresponding delay cost.
+Feel free to experiment with different input files and algorithm parameters to achieve optimal results.
+Please let me know if you need any further assistance.
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,19 +1,95 @@
 import random,csv
 
-def generate_input_data():
-    input_data_file_name = "data\\input.csv"
+# Files:
+vectors_initial_data = "data\\vectors_initial_data.csv"
+jobs_data = "data\\jobs_data.csv"
 
-    def get_randomize_jobs(jobs_amount = 6):
-        jobs_list = list(range(1, jobs_amount + 1))
-        random.shuffle(jobs_list)
-        return jobs_list
+nutation_interval = 2
+nutation_counter = 0
 
-    def export_list(list,file_name = input_data_file_name):
-        with open(file_name,'a' ,newline='') as f:
-            writer = csv.writer(f)
+def get_randomize_jobs(jobs_amount=10):
+    jobs_list = list(range(1, jobs_amount + 1))
+    random.shuffle(jobs_list)
+    return jobs_list
+
+def generate_input_data(vectors=1):
+
+    def export_list(list,file_name = vectors_initial_data):
+        with open(file_name,'a' ,newline='') as file:
+            writer = csv.writer(file)
             writer.writerow(list)
 
-    jobs_list = get_randomize_jobs()
-    export_list(jobs_list)
+    for _ in range(vectors):
+        jobs_list = get_randomize_jobs()
+        export_list(jobs_list)
 
-generate_input_data()
+def get_job_process_time(job_id):
+    with open(jobs_data, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if int(row['job_id']) == job_id:
+                return int(row['process_time'])
+
+    return None  # Return None if job ID is not found
+
+def get_job_delivery_time(job_id):
+    with open(jobs_data, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if int(row['job_id']) == job_id:
+                return int(row['delivery_time'])
+
+    return None  # Return None if job ID is not found
+
+def get_total_days_late(jobs_order_list):
+    days_late = 0
+    curr_process_time = 0
+    for job_id in jobs_order_list:
+        process_time = get_job_process_time(job_id)
+        delivery_time = get_job_delivery_time(job_id)
+
+        curr_process_time += process_time
+
+        diff = curr_process_time - delivery_time
+
+        if diff > 0: days_late += diff
+    return days_late
+
+def get_hybrid_mask_list(jobs_amount=10):
+    #output optional example: [1,0,0,0,1,0,0,1,0,1]
+    mask_list = []
+    for _ in range(jobs_amount):
+        mask_list.append(random.choice([0, 1]))
+    return mask_list
+
+
+########################################## Need to rewrite:###########################################################################
+def encode_jobs_vector(jobs_list):
+    encoded_vector = []
+
+    for i, job in enumerate(jobs_list):
+        count = sum(1 for j, prev_job in enumerate(jobs_list[:i]) if prev_job > job)
+        encoded_vector.append(count)
+
+    return encoded_vector
+
+# def decode_jobs_vector(encoded_list):
+
+###
+# Gets the user input for the amount of initial vectors to start with:
+
+# vectors = int(input("Specify how many job vectors you want to add: "))
+# generate_input_data(vectors)
+###
+
+############################## TESTS ##############################
+# Example usage - get_total_days_late()
+
+ranJobs = get_randomize_jobs()
+print(f"Jobs random list: {ranJobs}")
+
+# print(f"Days late: {get_total_days_late(ranJobs)}")
+
+encoded_list = encode_jobs_vector([2,1,6,4,5,3])
+print(f"Encoded list: {encoded_list}")
+
