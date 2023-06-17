e0c1a56fc9a67b66ba53578816841efa598bfb00
tamirzitman <tamirzitman@gmail.com>
2023-06-17 23:32:24 +0300

    completed functionality

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -1,12 +1,16 @@
-import random,csv,sys,os
+import random,csv,sys,os,math
 from operator import itemgetter
 
 
 # Script Variables:
+as_is_ratio = (1/3)
+hybrid_ratio = (2/3)
 mutation_rate = 5 # precentage of soulutions to be mutate
-population_size = 100
+jobs_amount = 10
+max_generations = 100
+no_imporve_threshold = 5
 
-def get_hybrid_mask_list(jobs_amount=10):
+def get_hybrid_mask_list(jobs_amount=jobs_amount):
     mask_list = []
     for _ in range(jobs_amount):
         mask_list.append(random.choice([0, 1]))
@@ -31,21 +35,44 @@ if not os.path.exists(directory):
 
 jobs_data = "data\\jobs_data.csv"
 
-def get_randomize_jobs(jobs_amount=10):
+def get_vector_array_by_id(vector_id, file_name=vectors_initial_data):
+    with open(file_name, 'r') as file:
+        reader = csv.reader(file)
+        row_id = 1
+        for row in reader:
+            if row_id == vector_id:
+                int_arr = list(map(int, row))
+                return int_arr
+            row_id += 1
+    return []  # Return an empty list if the vector_id is not found
+
+def get_randomize_jobs(jobs_amount=jobs_amount):
     jobs_list = list(range(1, jobs_amount + 1))
     random.shuffle(jobs_list)
     return jobs_list
 
-def generate_input_data(vectors=1):
+def set_input_data(fresh_list,random_vectors,clean_list = False):
 
-    def export_list(list,file_name = vectors_initial_data):
-        with open(file_name,'a' ,newline='') as file:
+    def export_list(data_list,file_name = vectors_initial_data,clean_csv=False):
+        operation = 'a' # append
+        if clean_csv: operation = 'w'
+        with open(file_name, operation ,newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(list)
+            if data_list:
+                writer.writerow(data_list)
 
-    for _ in range(vectors):
-        jobs_list = get_randomize_jobs()
-        export_list(jobs_list)
+    if clean_list and fresh_list:
+        #clean the exsiting list:
+        export_list(data_list= "",clean_csv=True)
+        #popuate with fresh list
+        for jobs_list in fresh_list:
+            export_list(jobs_list)
+        return
+
+    if random_vectors:
+        for _ in range(random_vectors):
+            jobs_list = get_randomize_jobs()
+            export_list(jobs_list)
 
 
 def get_job_process_time(job_id):
@@ -111,6 +138,11 @@ def get_ranked_array(array_of_lists):
 
     return ranked_array
 
+def get_best_solution(ranked_array):
+    vector_id = ranked_array[-1]["vector_id"]
+    vector = get_vector_array_by_id(vector_id)
+    return {"vector":vector,"days_late":get_total_days_late(vector)}
+
 def get_rank_sum_of__ranked_array(rank_array_of_lists):
     sum = 0
     for item in rank_array_of_lists:
@@ -160,6 +192,21 @@ def decode_jobs_vector(encoded_list):
 
     return decoded_jobs_list
 
+def get_hybrid_vector(vector0,vector1):
+    hybrid_vector = []
+
+    encode0_list = encode_jobs_vector(vector0)
+    encode1_list = encode_jobs_vector(vector1)
+
+    for (en0_job, en1_job,current_parent) in zip(encode0_list,encode1_list,hybrid_mask):
+        chosen_job_to_append = en0_job
+        if current_parent == 1:
+            chosen_job_to_append = en1_job
+
+        hybrid_vector.append(chosen_job_to_append)
+
+    return decode_jobs_vector(hybrid_vector)
+
 def get_mutate_list(jobs_list):
     # Generate two distinct random indices
     index1, index2 = random.sample(range(len(jobs_list)), 2)
@@ -167,32 +214,112 @@ def get_mutate_list(jobs_list):
     jobs_list[index1], jobs_list[index2] = jobs_list[index2], jobs_list[index1]
     return jobs_list
 
+def get_choosen_vector(cum_distributed_array,only_vector = False):
+    random_prob = random.random()
+    ran_choosen_item = next(item for item in cum_distributed_array if item["low_prob"] < random_prob< item["high_prob"])
+    choosen_vector = get_vector_array_by_id(ran_choosen_item["vector_id"])
+    if only_vector:
+        return choosen_vector
+    return {"vector_id":ran_choosen_item["vector_id"], "vector":choosen_vector}
+
+def test_vector_id_in_list_of_lists(vector_id,array_of_arrays):
+    for array in array_of_arrays:
+        if get_vector_array_by_id(vector_id) == array : return True
+    return False
+
+def get_new_generation(cum_distributed_array,as_is_ratio = as_is_ratio ,hybrid_ratio = hybrid_ratio,mutation_rate = mutation_rate):
+    new_gen_array = []
+    total_vectors = len(cum_distributed_array)
+
+    as_is_vectors_amount = math.ceil(as_is_ratio * total_vectors)
+    hybrid_vectors_amount = math.ceil(hybrid_ratio * total_vectors)
+
+    while( as_is_vectors_amount + hybrid_vectors_amount > total_vectors):
+        as_is_vectors_amount -= 1
+
+    mutation_amount = math.ceil(mutation_rate/100 * total_vectors)
+
+    while( as_is_vectors_amount + hybrid_vectors_amount):
+
+        vector_dict = get_choosen_vector(cum_distributed_array)
+        vector = vector_dict["vector"]
+        if as_is_vectors_amount > 0:
+            vector_id = vector_dict["vector_id"]
+            # Make sure the choosen one is not already in the new_gen
+            exsits = test_vector_id_in_list_of_lists(vector_id,new_gen_array)
+
+            if not exsits:
+                new_gen_array.append(vector)
+                as_is_vectors_amount -= 1
+            continue
+
+        if hybrid_vectors_amount > 0:
+            vec0 = vector
+            vec1 = get_choosen_vector(cum_distributed_array,True)
+            hybrid_vec = get_hybrid_vector(vec0,vec1)
+            new_gen_array.append(hybrid_vec)
+            hybrid_vectors_amount -= 1
+            continue
+
+    # random mutation:
+    while mutation_amount > 0:
+        ran_vector = random.choice(new_gen_array) #.items()?
+        # Mutate the selected random vector
+        mutate_vector = get_mutate_list(ran_vector)
+
+        # Update the mutated sublist back to the new_gen_array
+        index = new_gen_array.index(ran_vector)
+        new_gen_array[index] = mutate_vector
+
+        mutation_amount -= 1
+        continue
+
+    return new_gen_array
+
 ###
 # Gets the user input for the amount of initial vectors to start with:
 
 vectors = int(input("Specify how many job vectors you want to add: "))
-generate_input_data(vectors)
+if vectors!=0 : set_input_data(fresh_list=[],random_vectors=vectors)
 
-main_arr = read_csv_to_array()
-print(f"Original Array: {main_arr}")
+generation_counter = 0
 
-ranked = get_ranked_array(main_arr)
-print(f"Ranked Array: {ranked}")
+while generation_counter < max_generations:
+    main_arr = read_csv_to_array()
+    # print(f"Original Array: {main_arr}")
 
-rank_sum = get_rank_sum_of__ranked_array(ranked)
-print(f"Rank Sum: {rank_sum}")
-
-cum_distributed = get_cum_dist_array(ranked,rank_sum)
-print(f"Cumulative Distributed array: {cum_distributed}")
+    ranked = get_ranked_array(main_arr)
+    # print(f"Ranked Array: {ranked}")
 
 
-###
+    best_solution = get_best_solution(ranked)
+    print(f"best_solution: {best_solution}")
+    best_solution = best_solution["days_late"]
+
+    rank_sum = get_rank_sum_of__ranked_array(ranked)
+    # print(f"Rank Sum: {rank_sum}")
+
+    cum_distributed = get_cum_dist_array(ranked,rank_sum)
+    # print(f"Cumulative Distributed array: {cum_distributed}")
+
+    new_gen_array = get_new_generation(cum_distributed)
+    # print(f"New Generation array: {new_gen_array}")
+
+    set_input_data(fresh_list = new_gen_array,clean_list=True,random_vectors=0)
+    generation_counter += 1
+
 
 ############################## TESTS ##############################
-# Example usage - get_total_days_late()
 
-# ranJobs = get_randomize_jobs()
-# print(f"Jobs random list: {ranJobs}")
+# ranJobs0 = get_randomize_jobs()
+# ranJobs1 = get_randomize_jobs()
+# print(f"Jobs0 random list: {ranJobs0}")
+# print(f"Jobs1 random list: {ranJobs1}")
+
+# print(f"hybrid mask is: {hybrid_mask}")
+
+# hyb = get_hybrid_vector(ranJobs0,ranJobs1)
+# print(f"Hybrid list: {hyb}")
 
 # print(f"Days late: {get_total_days_late(ranJobs)}")
 
